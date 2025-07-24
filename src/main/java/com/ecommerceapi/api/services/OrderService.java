cb3485299da337c90601fcc71f57547e7ce07994
José Antonio <josejr017@gmail.com>
2025-07-24 14:48:10 -0300

    checkout method and auxiliary functions

---
+++ src/main/java/com/ecommerceapi/api/services/OrderService.java
@@ -0,0 +1,147 @@
+package com.ecommerceapi.api.services;
+
+import com.ecommerceapi.api.domain.cart.Cart;
+import com.ecommerceapi.api.domain.cartItem.CartItem;
+import com.ecommerceapi.api.domain.order.Order;
+import com.ecommerceapi.api.domain.order.OrderResponseDTO;
+import com.ecommerceapi.api.domain.orderItem.OrderItem;
+import com.ecommerceapi.api.domain.orderItem.OrderItemResponseDTO;
+import com.ecommerceapi.api.domain.payment.Payment;
+import com.ecommerceapi.api.domain.payment.PaymentResponseDTO;
+import com.ecommerceapi.api.domain.product.Product;
+import com.ecommerceapi.api.domain.user.User;
+import com.ecommerceapi.api.repositories.*;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Transactional
+    public OrderResponseDTO checkout(UUID userId) {
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Cart not found"));
+
+        List<CartItem> cartItems = cart.getCartItems();
+        if(cartItems.isEmpty()){
+          throw new IllegalArgumentException("Cart's empty");
+        }
+
+        Order order = createOrder(userId);
+
+        List<OrderItem> orderItems = createOrderItems(cartItems, order);
+
+        // Calcular totalAmount
+        BigDecimal totalAmount = orderItems.stream()
+                .map(orderItem -> orderItem.getUnitPrice().multiply(BigDecimal.valueOf(orderItem.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        order.setTotalAmount(totalAmount);
+        order.setOrderItemList(orderItems);
+
+        orderRepository.save(order);
+
+        Payment payment = createPayment(order, totalAmount);
+
+        order.setStatus("PAID");
+        orderRepository.save(order);
+
+        // Limpar carrinho
+        cartItemRepository.deleteByCartId(cart.getId());
+
+        return toOrderResponseDTO(order, payment);
+
+    }
+
+    // Criar Order
+    private Order createOrder(UUID userId) {
+        Order order = new Order();
+        order.setUser(new User(userId));
+        order.setStatus("PENDING");
+        order.setCreatedAt(new Date());
+        return order;
+    }
+
+    // Criar OrderItems com snapshot dos preços
+    private List<OrderItem> createOrderItems(List<CartItem> cartItems, Order order) {
+        return cartItems.stream()
+                .map(cartItem -> {
+                    Product product = cartItem.getProduct();
+                    if (product.getStock() < cartItem.getQuantity()) {
+                        throw new IllegalArgumentException("Insufficient stock for product: " + product.getName());
+                    }
+
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setOrder(order);
+                    orderItem.setProduct(product);
+                    orderItem.setQuantity(cartItem.getQuantity());
+                    orderItem.setUnitPrice(product.getPrice());
+
+                    product.setStock(product.getStock() - cartItem.getQuantity());
+                    productRepository.save(product);
+
+                    return orderItem;
+                }).collect(Collectors.toList());
+    }
+
+    // Criar Payment
+    private Payment createPayment(Order order, BigDecimal totalAmount) {
+        Payment payment = new Payment();
+        payment.setOrder(order);
+        payment.setPaymentDate(new Date());
+        payment.setStatus("COMPLETED");
+        payment.setAmount(totalAmount);
+
+        paymentRepository.save(payment);
+
+        return payment;
+    }
+
+    private OrderResponseDTO toOrderResponseDTO(Order order, Payment payment) {
+        // Payment payment = paymentRepository.findByOrderId(order.getId()).orElse(null);
+        return new OrderResponseDTO(
+                order.getId(),
+                order.getTotalAmount(),
+                order.getStatus(),
+                order.getCreatedAt(),
+                order.getUser().getId(),
+                order.getOrderItemList().stream()
+                        .map(item -> new OrderItemResponseDTO(
+                                item.getId(),
+                                item.getQuantity(),
+                                item.getUnitPrice(),
+                                item.getProduct().getId(),
+                                item.getProduct().getName()
+                        ))
+                        .collect(Collectors.toList()),
+                payment != null ? new PaymentResponseDTO(
+                        payment.getId(),
+                        payment.getAmount(),
+                        payment.getStatus(),
+                        payment.getPaymentDate()
+                ) : null
+        );
+    }
+}
