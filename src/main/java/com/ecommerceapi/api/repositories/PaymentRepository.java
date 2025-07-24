7f04fe10452c4e7d2614422878452c4f076bed9c
José Antonio <josejr017@gmail.com>
2025-07-24 18:15:32 -0300

    findByOrderId Query

--- src/main/java/com/ecommerceapi/api/repositories/PaymentRepository.java
+++ src/main/java/com/ecommerceapi/api/repositories/PaymentRepository.java
@@ -2,8 +2,16 @@ package com.ecommerceapi.api.repositories;
 
 import com.ecommerceapi.api.domain.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+
+    @Query("SELECT p FROM Payment p " +
+            "LEFT JOIN p.order o " +
+            "WHERE o.id = :order_id")
+    Optional<Payment> findByOrderId(@Param("order_id") UUID orderId);
 }
