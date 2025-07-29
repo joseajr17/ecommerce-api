41cc51cb5ea71179879b7f533ec4182e5c9a8f51
José Antonio <josejr017@gmail.com>
2025-07-29 10:29:07 -0300

    findByEmail Query

--- src/main/java/com/ecommerceapi/api/repositories/UserRepository.java
+++ src/main/java/com/ecommerceapi/api/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.ecommerceapi.api.repositories;
 
 import com.ecommerceapi.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByEmail(String email);
 }
